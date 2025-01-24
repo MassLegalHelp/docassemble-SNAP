9e1e347802d44660335d5c056ebb7f4bccc7fc62
Toby Fey <tobyfey@protonmail.com>
2025-01-24 13:01:00 -0500

    updatefpl2025

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -52,8 +52,7 @@ setup(name='docassemble.SNAP',
       author_email='rhahn@mlri.org',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/SNAP/', package='docassemble.SNAP'),
